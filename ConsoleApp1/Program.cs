ca47421ba4dc958688457606f21e76af30bcb342
Sander Buruma <sander.buruma@horganizer.com>
2020-02-06 23:14:27 +0100

    Finds random prime numbers of so many bites as specified by the user (save for a minimum and maximum)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Numerics;
 
 namespace ConsoleApp1
@@ -7,31 +7,88 @@ namespace ConsoleApp1
     class Program
     {
         /// <summary>
-        /// A program which should spit out reall big primes.
+        /// The configuration Enum
+        /// </summary>
+        enum Config
+        {
+            /// <summary>
+            /// How many bytes to let the BigIntegers be
+            /// </summary>
+            MaxPrimeSizeInBytes = 100_000,
+            MinPrimeSizeInBytes = 16,
+        }
+        /// <summary>
+        /// A program which should spit out really big primes.
         /// </summary>
         /// <param name="args"></param>
         static void Main()
         {
             Random random = new Random();
-
+            int bytesSize;
+            Console.WriteLine("Please input the number of bytes to used by each individual prime (between {1} - {0}):", ((int)Config.MaxPrimeSizeInBytes).ToString("N0"), ((int)Config.MinPrimeSizeInBytes).ToString("N0"));
             while (true)
             {
-                BigInteger newInt = GenerateRandomBigInt(random);
+                if (!int.TryParse(Console.ReadLine(), out bytesSize))
+                {
+                    Console.WriteLine("Please input a valid number: ");
+                }
+                else if (bytesSize > (int)Config.MaxPrimeSizeInBytes)
+                {
+                    Console.WriteLine("Please input a number that's less than {0}", ((int)Config.MaxPrimeSizeInBytes).ToString("N0"));
+                }
+                else if (bytesSize < (int)Config.MinPrimeSizeInBytes)
+                {
+                    Console.WriteLine("Please input a number that's greater than {0}", ((int)Config.MinPrimeSizeInBytes).ToString("N0"));
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.Clear();
+
+            BigInteger numberToCheck;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (true)
+            {
+                notPrimeB:;
+                numberToCheck = GenerateRandomBigInt(random, bytesSize);
+                foreach (int prime in new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 91, 97, 101 })
+                {
+                    //Simple relatively inexpensive check used to exclude some easy pickouts
+                    if (numberToCheck % prime == 0) goto notPrimeB;
+                }
+                for (int j = 9; j < 11; j++)
+                {
+                    //Fermat test
+                    if (BigInteger.ModPow(j, numberToCheck - 1, numberToCheck) != 1)
+                    {
+                        goto notPrimeB;
+                    }
+                }
+                sw.Stop();
+                Console.Clear();
+                Console.WriteLine(numberToCheck.ToString("N0") + " is prime");
+                Console.WriteLine(sw.ElapsedMilliseconds.ToString("N0") + " ms used\n");
+                Console.WriteLine("press key to continue");
+                Console.ReadKey();
+                Console.WriteLine("----------------------calculating prime----------------------");
+                sw.Restart();
             }
         }
-
-        static private BigInteger GenerateRandomBigInt(Random random)
+        /// <summary>
+        /// Generates only posiive bigints
+        /// </summary>
+        /// <param name="random"></param>
+        /// <returns></returns>
+        static private BigInteger GenerateRandomBigInt(Random random, int bytesSize)
         {
-            byte[] bytes = new byte[(int)Config.PrimeSizeInBytes];
+            byte[] bytes = new byte[bytesSize+1];
             random.NextBytes(bytes);
-            bytes[(int)Config.PrimeSizeInBytes - 1] = byte.Parse("0");
+            bytes[bytesSize] = byte.Parse("0");
 
             return new BigInteger(bytes);
         }
-    }
-
-    enum Config
-    {
-        PrimeSizeInBytes = 65
     }
 }
