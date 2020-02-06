e064b49ddc32c5d2aa569eac6f0c0dc6ee5dc684
Sander Buruma <sander.buruma@horganizer.com>
2020-02-06 21:18:49 +0100

    initial

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// A program which should spit out reall big primes.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main()
         {
-            
+            Random random = new Random();
+
+            while (true)
+            {
+                BigInteger newInt = GenerateRandomBigInt(random);
+            }
         }
+
+        static private BigInteger GenerateRandomBigInt(Random random)
+        {
+            byte[] bytes = new byte[(int)Config.PrimeSizeInBytes];
+            random.NextBytes(bytes);
+            bytes[(int)Config.PrimeSizeInBytes - 1] = byte.Parse("0");
+
+            return new BigInteger(bytes);
+        }
+    }
+
+    enum Config
+    {
+        PrimeSizeInBytes = 65
     }
 }
